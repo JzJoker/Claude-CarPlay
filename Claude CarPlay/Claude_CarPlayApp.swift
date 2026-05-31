f98e9ec84c66f1b7740e81f3307a4317fa1e7cf7
JzJoker <justinzhao1324@gmail.com>
2026-05-31 14:40:13 -0400

    Initial Commit

---
+++ Claude CarPlay/Claude_CarPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_CarPlayApp.swift
+//  Claude CarPlay
+//
+//  Created by Justin Zhao on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Claude_CarPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
